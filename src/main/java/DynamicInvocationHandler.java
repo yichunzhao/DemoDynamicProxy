578204270269f1e2d32423a557410b30a00c2d3b
yichunzhao <zhaoyichun@hotmail.com>
2020-05-22 01:42:58 +0200

    Initial commit

---
+++ src/main/java/DynamicInvocationHandler.java
@@ -0,0 +1,11 @@
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+
+public class DynamicInvocationHandler implements InvocationHandler {
+
+
+    public Object invoke(Object o, Method method, Object[] objects) throws Throwable {
+        return null;
+    }
+}
