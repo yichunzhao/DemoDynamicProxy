6b2f496ba4e6c72f4b50cffe577c845edd270e69
yichunzhao <zhaoyichun@hotmail.com>
2020-05-22 18:35:42 +0200

    jdk creating a dynamic proxy, wrapping an instance with another invoke method

--- src/main/java/DemoJdkDynamicProxy.java
+++ src/main/java/DemoJdkDynamicProxy.java
@@ -1,10 +1,21 @@
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.lang.reflect.Proxy;
+import java.util.Map;
+
 public class DemoJdkDynamicProxy {
     private static final Logger logger = LoggerFactory.getLogger(DemoJdkDynamicProxy.class);
 
     public static void main(String[] args) {
 
+        Map proxyInstance = (Map) Proxy.newProxyInstance(
+                DemoJdkDynamicProxy.class.getClassLoader(),
+                new Class[]{Map.class},
+                new DynamicInvocationHandler()
+        );
+
+        proxyInstance.put("hello", "world");
+        
     }
 }
--- src/main/java/DynamicInvocationHandler.java
+++ src/main/java/DynamicInvocationHandler.java
@@ -1,11 +1,16 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 
 public class DynamicInvocationHandler implements InvocationHandler {
 
+    private static Logger LOGGER = LoggerFactory.getLogger(DynamicInvocationHandler.class);
 
     public Object invoke(Object o, Method method, Object[] objects) throws Throwable {
-        return null;
+        LOGGER.info("Invoked method:{}", method.getName());
+
+        return 42;
     }
 }
